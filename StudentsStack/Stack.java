d1b18ab474d92f2dd61412a30c9af46525a7cfee
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-20 20:43:52 +0700

    update

--- StudentsStack/Stack.java
+++ StudentsStack/Stack.java
@@ -1,5 +1,33 @@
 package StudentsStack;
 
 public class Stack {
-    
+    Student [] stack;
+
+    int top , size;
+
+    Stack(int size){
+        this.size = size;
+        this.top = -1;
+        stack = new Student[size];
+    }
+
+    boolean isfull(){
+        return top == size-1;
+    }
+
+    boolean isEmpty (){
+        return top == -1;
+    }
+
+    public void push(Student data){
+        if (isfull()){
+            System.out.println("the Stack is already full");
+            return;
+        }
+        top++;
+        stack[top]=data;
+        System.out.println("the data of : '"+data.name+"' is pushed");
+    }
+
+    public
 }
