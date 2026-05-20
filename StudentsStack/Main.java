2ecdcff6d8431894649f98ec5cc26ffbe6a0abaf
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-20 21:13:22 +0700

    update

--- StudentsStack/Main.java
+++ StudentsStack/Main.java
@@ -1,5 +1,27 @@
 package StudentsStack;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter size of stack : ");
+        int size = in.nextInt();
+
+        Stack s = new Stack(size);
+
+        int choice;
+
+        do{
+            System.out.println("    ====MENU====");
+            System.out.println("1. Push data");
+            System.out.println("2. PoP data");
+            System.out.println("3. Peek top");
+            System.out.println("4. Delete all data");
+            System.out.println("========================");
+            System.out.println("Enter Choice : ");
+            
+        }
+    }
     
 }
