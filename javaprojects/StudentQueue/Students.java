9ff628c17ddd8cffff01d77336c529b6c470e04b
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-21 14:06:03 +0700

    updates

--- javaprojects/StudentQueue/Students.java
+++ javaprojects/StudentQueue/Students.java
@@ -1,5 +1,14 @@
 package javaprojects.StudentQueue;
 
 public class Students {
+    String nim, name, className,major;
+
+    Students (String nim, String name, String className, String major){
+        this.nim = nim;
+        this.name = name;
+        this.className = className;
+        this.major = major;
+        
+    }
     
 }
