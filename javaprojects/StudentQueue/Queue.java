4566bf87df9080e8cf64a8b04b4974a70a886f25
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-21 14:59:54 +0700

    updates

--- javaprojects/StudentQueue/Queue.java
+++ javaprojects/StudentQueue/Queue.java
@@ -12,4 +12,62 @@ public class Queue {
         queue = new Students[size];
 
     }
+
+    boolean isEmpty (){
+        return front == -1;
+    }
+
+    boolean isFull (){
+        return rear == size-1;
+    }
+
+    public void enqueue(Students data){
+        if (isFull()){
+            System.out.println("the Student Queue is full");
+            return;
+        }
+        if(front == -1){
+            front =0;
+            
+        }
+        rear++;
+        queue[rear] = data;
+        System.out.println("the data of '"+data.name+"' is Enqueued");
+
+    }
+
+    public void dequeue (){
+        if (isEmpty()){
+            System.out.println("Student Queue is Empty ");
+            return;
+        }
+
+        Students tmp = queue[front];
+
+        front++;
+
+        if(front > rear){
+            front = -1;
+            rear = -1;
+        }
+        System.out.println("the value removed"+tmp.name);
+    }
+
+    public void showFront (){
+        if (isEmpty()){
+            System.out.println("the Queue is Empty");
+            return;
+        }
+        System.out.println("the front is : "+queue[front]);
+
+    }
+
+    public void showRear(){
+        if (isEmpty()){
+            System.out.println("the Queue is Empty");
+            return;
+        }
+        System.out.println("the Rear is : "+queue[rear]);
+    }
+    
 }
