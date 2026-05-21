8317e76a7096ecbee8ad8d41624e80dc2bb54713
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-21 14:04:52 +0700

    updates

--- javaprojects/StudentQueue/Queue.java
+++ javaprojects/StudentQueue/Queue.java
@@ -1,5 +1,15 @@
 package javaprojects.StudentQueue;
 
 public class Queue {
+    Students [] queue;
+
+    int front,rear,size;
     
+    Queue(int size){
+        this.size = size;
+        this.front = -1;
+        this.size = -1;
+        queue = new Students[size];
+
+    }
 }
