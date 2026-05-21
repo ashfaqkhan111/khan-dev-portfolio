59c5d77e39e39a9bc42e686e59d8925f02e2b567
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-21 08:16:39 +0700

    update

---
+++ javaprojects/StudentStack/Student.java
@@ -0,0 +1,15 @@
+package StudentsStack;
+
+public class Student {
+    String studentNumber;
+    String name,className,major;
+
+    Student(String studentNumber, String name,String className, String major){
+        this.studentNumber = studentNumber;
+        this.name = name;
+        this.className = className;
+        this.major=major;
+    }
+    
+}
+   
