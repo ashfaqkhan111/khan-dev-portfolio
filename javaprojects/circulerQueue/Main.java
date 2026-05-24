1371f77421c0eb8bf69046915b31a50ccd29407c
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 11:24:16 +0700

    updates

--- javaprojects/circulerQueue/Main.java
+++ javaprojects/circulerQueue/Main.java
@@ -19,20 +19,55 @@ public class Main {
             System.out.println("3. Show Rear");
             System.out.println("4. Show Front");
             System.out.println("5. Erase Queue");
+            System.out.println("6. program ended");
             System.out.println("=================");
             System.out.println("Enter Choice : ");
             choice = in.nextInt();
             in.nextLine();
             
-            switch (q) {
-                case value:
-                    
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter your NIM : ");
+                    String nim = in.nextLine();
+
+                    System.out.print("Enter your Name : ");
+                    String name = in.nextLine();
+
+                    System.out.print("Enter your Class Name : ");
+                    String className = in.nextLine();
+
+                    System.out.print("Enter your Major : ");
+                    String major = in.nextLine();
+
+                    Students s = new Students(nim, name, className, major);
+                    q.enqueue(s);
+                    break;
+
+                case 2:
+                    q.dequeue();
+                    break;
+
+                case 3:
+                    q.showFront();
+                    break;
+
+                case 4:
+                    q.showRear();
+                    break;
+
+                case 5:
+                    q.eraseQueue();
+                    break;
+
+                case 6:
+                    System.out.println("program is ended ");
                     break;
             
                 default:
+                    System.out.println("program ended!!");
                     break;
             }
-        }
+        }while (choice != 6);
     }
     
 }
