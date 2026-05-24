c53cfd8bf050618b65da408a4965c36e22cdc464
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 15:10:01 +0700

    updates

---
+++ javaprojects/LinkedList/Node.java
@@ -0,0 +1,12 @@
+package javaprojects.LinkedList;
+
+public class Node {
+    Student data;
+    Node next;
+
+    Node (Student data){
+        this.data = data;
+        this.next = null;
+    }
+    
+}
