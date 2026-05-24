de59e44f8fa234e83bc6173cbc8dc6e0411bed31
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 15:09:40 +0700

    updates

---
+++ javaprojects/LinkedList/LinkList.java
@@ -0,0 +1,20 @@
+package javaprojects.LinkedList;
+
+public class LinkList {
+    Node head;
+
+    boolean isEmpty (){
+        return head == null;
+    }
+
+    public void addFirst (Student data){
+        Node newNode = new Node(data);
+
+        if (head == null){
+            head = newNode;
+        }else{
+            
+        }
+    }
+    
+}
