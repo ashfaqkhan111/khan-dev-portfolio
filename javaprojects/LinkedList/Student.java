3fb9a95c1d5b87f37ab817e2e5684fe041869944
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 15:10:10 +0700

    updates

---
+++ javaprojects/LinkedList/Student.java
@@ -0,0 +1,15 @@
+package javaprojects.LinkedList;
+
+public class Student {
+    String nim,name,className,major;
+
+    Student(String nim, String name, String className, String major){
+        this.nim = nim;
+        this.name = name;
+        this.className = className;
+        this.major = major;
+    }
+
+    
+    
+}
