4601e4aeb4c7e5ff4528c6aec44abac2858a5fca
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 10:57:26 +0700

    updates

--- javaprojects/circulerQueue/Main.java
+++ javaprojects/circulerQueue/Main.java
@@ -1,5 +1,38 @@
 package javaprojects.circulerQueue;
+import java.util.*;
 
 public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the size : ");
+        int size = in.nextInt();
+
+        CirculerQueue q = new CirculerQueue(size);
+
+        int choice;
+
+        do {
+            System.out.println("====MENU====");
+            System.out.println("1. Enqueue");
+            System.out.println("2. Dequeue");
+            System.out.println("3. Show Rear");
+            System.out.println("4. Show Front");
+            System.out.println("5. Erase Queue");
+            System.out.println("=================");
+            System.out.println("Enter Choice : ");
+            choice = in.nextInt();
+            in.nextLine();
+            
+            switch (q) {
+                case value:
+                    
+                    break;
+            
+                default:
+                    break;
+            }
+        }
+    }
     
 }
