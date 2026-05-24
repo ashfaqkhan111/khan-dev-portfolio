f63939935de0e74a55217f6179aafd6606f1f04a
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 16:00:08 +0700

    updates

--- javaprojects/LinkedList/LinkList.java
+++ javaprojects/LinkedList/LinkList.java
@@ -13,7 +13,49 @@ public class LinkList {
         if (head == null){
             head = newNode;
         }else{
-            
+          newNode.next = head;
+          head = newNode;  
+        }
+    }
+
+    public void addLast (Student data){
+        Node newNode = new Node(data);
+        if (isEmpty()){
+            head = newNode;
+        }else{
+           Node currNode = head;
+           
+           while(currNode.next != null){
+            currNode = currNode.next;
+           }
+
+           currNode = newNode;
+        }
+
+    }
+
+    public void print(){
+        if (isEmpty()){
+            System.out.println("The Stack is Empty");
+            return;
+        }else {
+            Node currnNode = head;
+
+            while(currnNode != null){
+                currnNode.data.print();
+
+                currnNode = currnNode.next;
+            }
+        }
+
+    }
+
+    public void removeFirst(){
+        if (isEmpty()){
+            System.out.println("the stack is already empty !!");
+            return;
+        }else{
+            head = head.next;
         }
     }
     
