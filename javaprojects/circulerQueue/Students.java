9b853972928fa6a11c3ffc40d77bbef0bb15c99c
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-05-24 10:09:11 +0700

    updates

--- javaprojects/circulerQueue/Students.java
+++ javaprojects/circulerQueue/Students.java
@@ -1,5 +1,19 @@
 package javaprojects.circulerQueue;
 
 public class Students {
+    int nim;
+    String name,className,major;
+
+    Students(int nim, String name, String className, String major){
+        this.nim = nim;
+        this.name = name;
+        this.className = className;
+        this.major = major;
+
+    }
+
+    public void print(){
+        System.out.println(nim+" "+name+" "+className+" "+major);
+    }
     
 }
