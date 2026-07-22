c641ead37f85428b7cdc36a3983ef8d84deb42ec
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-07-22 15:59:28 +0700

    uodated

--- library-management-system/booklist.php
+++ library-management-system/booklist.php
@@ -11,6 +11,9 @@ $conn = getConnection();
 
 // }
 
+$categoryRsult = mysqli_query($conn, "
+SELECT category_id, category_name FROM categories ORDER BY category_name ASC");
+
 ?>
 
 <!DOCTYPE html>
@@ -45,17 +48,102 @@ $conn = getConnection();
                     </li>
 
                     <li>
-                        <a href="booklist.php"></a>
+                        <a href="booklist.php">
                         <i class="fa-solid fa-book"></i>
+                        <span>Books</span>
+                        </a>
+                    </li> 
+
+                    <li>
+                        <a href="members.php">
+                            <i class="fa-solid fa-users"></i>
+                            <span>Members</span>
+                        </a>
                     </li>
 
-                    
+                    <li>
+                        <a href="borrow.php">
+                        <i class="fa-solid fa-book-open-reader"></i>
+                        <span>Borrow</span>
+                        </a>
+                    </li>
 
+                    <li>
+                        <a href="return.php">
+                        <i class="fa-solid fa-rotate-left"></i>
+                        <span>Return</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="fine.php">
+                        <i class="fa-solid fa-money-bill"></i>
+                        <span>Fine Management</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="reports.php">
+                        <i class="fa-solid fa-chart-column"></i>
+                        <span>Reports</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="settings.php">
+                            <i class="fa-solid fa-gear"></i>
+                            <span>Settings</span>
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="logout.php">
+                            <i class="fa-solid fa-right-from-bracket"></i>
+                            <span>logout</span>
+                        </a>
+                    </li>
+                    
                 </ul>
 
             </aside>
 
             <main class="main-content">
+                <header class="topbar">
+                    <h2>Book Management</h2>
+                    <div class="profile">
+                        <i class="fa-reguler fa-bell"></i>
+                        <span><?php echo htmlspecialchars($_SESSION['full_name']); ?></span>
+
+                    </div>
+
+                </header>
+
+                <section class="book-management">
+                    <div class="toolbar">
+                        <button class="add-book-btn">
+                            <i class="fa-solid fa-square-plus"></i>
+                            Add Book 
+                        </button>
+
+                        <div class="search-box">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <input type="text" name="search" placeholder="Search by title, ISBN, or author.....">
+
+                        </div>
+
+                        <select name="category" class="category-filter" id="">
+                            <option value="">All Categories</option>
+                            <?php while($category = mysqli_fetch_assoc($categoryRsult)){ ?>
+                            <option value="" <?php echo $category['category_id']; ?>></option>
+
+                            <?php echo htmlspecialchars($category['category_name']); ?>
+
+                           <?php }?>
+                        </select>
+
+                    </div>
+
+                </section>
 
             </main>
 
