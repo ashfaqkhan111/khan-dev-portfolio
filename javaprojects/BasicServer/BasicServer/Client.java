c8ecfcb17fc330332d90c27a4e0417e65e1ada1f
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-08-31 12:17:36 +0700

    client

--- javaprojects/BasicServer/BasicServer/Client.java
+++ javaprojects/BasicServer/BasicServer/Client.java
@@ -1,3 +1,38 @@
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+
 public class Client {
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 5000;
+
+        try (
+            Socket socket = new Socket(host, port);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            
+            PrintWriter output = new PrintWriter (socket.getOutputStream(), true);
+            Scanner in = new Scanner(System.in);
+        ){
+            System.out.println("Connected to server ");
+            while (true) {
+                System.out.println("You : ");
+                String message = in.nextLine();
+
+                output.println(message);
+
+                String response = input.readLine();
+                System.out.println(response);
+
+                if(message.equalsIgnoreCase("bye"));
+                break;
+                
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
     
 }
