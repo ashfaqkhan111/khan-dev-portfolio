87a8a1b3ff1365a613798137e4ecd8605ad79fc4
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-09-10 13:39:38 +0700

    update

---
+++ C++/basic programming/oddEven.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int x;
+
+	cout <<"Enter the number : " <<endl;
+	cin >>x;
+
+	if(x%2 == 0){
+	cout <<"the number is even" <<endl;
+	}
+	else {
+	cout <<"the number is odd"<<endl;
+	}
+return 0;
+
+}
