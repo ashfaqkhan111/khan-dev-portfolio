9009c2de00f0c1430f307ae629ce5a88df7b5192
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-09-13 15:36:07 +0700

    multaple table

---
+++ C++/basic programming/multiplicationTable.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int number;
+	
+	cout<<"Enter Number : ";
+	cin >> number;
+
+	for (int i = 1; i<=10; i++){
+	
+	cout<<number <<"x"<<i<<"="<<number *i<<endl;
+	
+	}
+return 0;
+
+}
