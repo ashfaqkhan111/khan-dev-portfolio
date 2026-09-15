4269428b451eb163ec3afc8f695079315b9e9630
ashfaqkhan111 <ahfaqkhan97@gmail.com>
2026-09-15 09:02:03 +0700

    main server start test

---
+++ internship-management-system/java/src/Main.java
@@ -0,0 +1,21 @@
+import com.sun.net.httpserver.HttpServer;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class Main{
+
+	public static void main (String [] args) throws IOException{
+
+	HttpServer server = HttpServer.create(new InetSocketAddress(8080),0);
+
+	server.createContext("/", exchange -> { String response = "Internshio Management system backend is running";
+	exchange.sendResponseHeaders(200, response.length());
+	exchange.getResponseBody().write(response.getBytes());
+	exchange.getResponseBody().close(); });
+
+	server.start();
+	System.out.println("Server stsrt at http://localhost:8080");
+
+	}
+
+}
